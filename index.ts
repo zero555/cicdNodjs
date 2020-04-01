3e6643241356de6e2b6bc7d754d3e9426a4670a3
Christian Coenen <coenen.christian@outlook.com>
2020-04-01 09:44:37 +0200

    project init

---
+++ index.ts
@@ -0,0 +1,32 @@
+import * as http from 'http';
+import * as url from 'url';
+import * as querystring from 'querystring';
+
+const hostname:string = 'localhost';
+const port:number = 3000;
+
+const server:http.Server = http.createServer((request, response) => {
+    const urlObject:url.Url = url.parse(request.url);
+    const searchParamsString:string = urlObject.query as string;
+    const searchParamsJSON:any = querystring.parse(searchParamsString);
+    const lowestNumber:number = Number(searchParamsJSON.von);
+    const highestNumber:number = Number(searchParamsJSON.bis);
+
+    let table = '<table><tr><th>n</th><th>n<sup>2</sup></th>';
+    for (let i = lowestNumber; i <= highestNumber; i++) {
+        table += `<tr><th>${i}</th><th>${i*i}</th>`;
+    }
+    table += '</table>';
+
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'text/html');
+
+    response.write('<html><head><title>Squared numbers</title></head><body>');
+    response.write('<style type="text/css"> table, td, th { border-collapse: collapse; border: 1px solid; }' +
+        'tr:not(:first-child) { text-align: right } </style>');
+    response.write(`<h1>Quadratzahlen ${lowestNumber}<sup>2</sup> bis ${highestNumber}<sup>2</sup></h1>`);
+    response.write(`${table}`);
+    response.end('</body></html>');
+}).listen(port,hostname,() => {
+    console.log(`Server gestartet auf http://${hostname}:${port}`);
+});
